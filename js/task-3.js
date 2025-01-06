56070118d685e1f4035d57106c2d1ef50d2de050
MaxPress <maxpress96@ukr.net>
2025-01-06 12:18:10 +0200

    add js code

--- js/task-3.js
+++ js/task-3.js
@@ -1,17 +1,16 @@
-function getElementWidth(content, padding, border) {
+function checkForSpam(message) {
+    const spam = `spam`;
+    const sale = `sale`;
+
+    const messageLower = message.toLowerCase();
     
-    const contentWidth = parseFloat(content); 
-    
-    const paddingWidth = parseFloat(padding); 
-
-    const borderWidth = parseFloat(border);
-
-    return contentWidth + (paddingWidth * 2) + (borderWidth * 2);
-
+    return messageLower.includes(spam) || messageLower.includes(sale);
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); 
-
-console.log(getElementWidth("60px", "12px", "8.5px")); 
-
-console.log(getElementWidth("200px", "0px", "0px"));
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
